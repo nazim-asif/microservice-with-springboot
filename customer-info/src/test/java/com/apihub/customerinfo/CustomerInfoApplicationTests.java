a01d0a93e8600a26ae880f6011ebf3be75f7eaab
nazim-asif <imnazimuddinasif@gmail.com>
2023-08-08 14:52:17 +0600

    adding test

--- customer-info/src/test/java/com/apihub/customerinfo/CustomerInfoApplicationTests.java
+++ customer-info/src/test/java/com/apihub/customerinfo/CustomerInfoApplicationTests.java
@@ -1,13 +1,36 @@
 package com.apihub.customerinfo;
 
+import com.apihub.customerinfo.abastruction.CustomerService;
+import com.apihub.customerinfo.client.IntegrationClient;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CustomerInfoApplicationTests {
 
+    @Autowired
+    private IntegrationClient integrationClient;
+
+    @Autowired
+    private CustomerService customerService;
+
     @Test
     void contextLoads() {
     }
+
+    @Test
+    void integrateClient(){
+        assertThat(integrationClient.getCustomerInfo()).isNotNull();
+    }
+
+    @Test
+    void getCustomerInfo(){
+        assertThat(customerService.getInfo()).isNotNull();
+    }
+
+
 
 }
